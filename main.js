c0137251772b4ccb2ab8b13fcff8760e1e5f9d09
Idowu Adekale <idowu.s.adekale@gmail.com>
2022-05-14 09:08:18 +0100

    still implementing some code

--- main.js
+++ main.js
@@ -112,3 +112,25 @@ function mySnack() {
     x.className = "show";
     setTimeout(function() { x.className = x.className.replace("show", ""); }, 3000);
 }
+
+// Modal Box
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("my-Img");
+var modalImg = document.getElementById("img-01");
+var captionText = document.getElementById("caption");
+img.onclick = function() {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
